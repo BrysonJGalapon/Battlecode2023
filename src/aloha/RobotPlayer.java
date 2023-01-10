eca10ab5f53bfdd1f8ee7b74c048d4a6856d4548
Bryson Galapon <bgalapon@google.com>
2023-01-10 20:16:53 +0000

    removed unused fields in RobotPlayer

--- src/aloha/RobotPlayer.java
+++ src/aloha/RobotPlayer.java
@@ -17,32 +17,12 @@ import aloha.robots.*;
  * is created!
  */
 public strictfp class RobotPlayer {
-
-  public static final Direction[] directions = {
-        Direction.NORTH,
-        Direction.NORTHEAST,
-        Direction.EAST,
-        Direction.SOUTHEAST,
-        Direction.SOUTH,
-        Direction.SOUTHWEST,
-        Direction.WEST,
-        Direction.NORTHWEST,
-    };
-
     /**
      * We will use this variable to count the number of turns this robot has been alive.
      * You can use static variables like this to save any information you want. Keep in mind that even though
      * these variables are static, in Battlecode they aren't actually shared between your robots.
      */
     static int turnCount = 0;
-
-    /**
-     * A random number generator.
-     * We will use this RNG to make some random moves. The Random class is provided by the java.util.Random
-     * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
-     * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
-     */
-    static final Random rng = new Random(6147);
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
